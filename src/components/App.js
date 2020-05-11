a324393cc31bae7393614f5e6aaa48e78f9ab778
danutz87 <danielghimpu@yahoo.com>
2020-05-11 17:03:04 +0100

    React exercise

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(newNote => (
+        <Note
+          key={newNote.key}
+          title={newNote.title}
+          content={newNote.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
